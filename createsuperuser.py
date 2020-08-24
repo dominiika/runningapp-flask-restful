547b26b7344f959f32dae2151192b477a0e70729
Dosia <dominika.gajda@gmail.com>
2020-08-24 12:07:45 +0200

    Update createsuperuser.py to use SQLAlchemy

--- createsuperuser.py
+++ createsuperuser.py
@@ -1,5 +1,6 @@
-import sqlite3
 from werkzeug.security import generate_password_hash
+from run import app
+from runningapp.models.user import UserModel, UserProfileModel
 
 
 class Superuser:
@@ -8,50 +9,20 @@ class Superuser:
     def __init__(self, username, password):
         self.username = username
         self.password = password
-        self.db = "runningapp/data.db"
 
     def create_admin(self):
-        """Create the admin"""
-        self._create_user()
-        self._create_user_profile()
-        self._get_user_id()
-        return "Superuser created successfully"
-
-    def _create_user(self):
-        """Create a record to populate the users table"""
-        connection = sqlite3.connect(self.db)
-        cursor = connection.cursor()
-        query = "INSERT INTO users (username, password, is_admin, is_staff) VALUES (?, ?, ?, ?)"
-        cursor.execute(
-            query, (self.username, generate_password_hash(self.password), True, True)
-        )
-        connection.commit()
-        connection.close()
-
-    def _create_user_profile(self):
-        """Create a record to populate the user_profiles table"""
-        user_id = self._get_user_id()
-        connection = sqlite3.connect(self.db)
-        cursor = connection.cursor()
-        query = (
-            "INSERT INTO user_profiles "
-            "(user_id, gender, age, height, weight, bmi, daily_cal, trainings_number, kilometers_run) "
-            "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"
-        )
-        cursor.execute(query, (user_id, "Female", 25, 170, 55, 0, 0, 0, 0))
-        connection.commit()
-        connection.close()
-
-    def _get_user_id(self):
-        """Find the id in the users table to make a relation to user_profiles table possible"""
-        connection = sqlite3.connect(self.db)
-        cursor = connection.cursor()
-        query = "SELECT * FROM users WHERE username=?"
-        result = cursor.execute(query, (self.username,))
-        user_id = result.fetchone()[0]
-        connection.commit()
-        connection.close()
-        return user_id
+        """Create a user model and a user profile model and save them to the database"""
+        with app.app_context():
+            hashed_pass = generate_password_hash(self.password)
+            user = UserModel(
+                username=self.username,
+                password=hashed_pass,
+                is_admin=True,
+                is_staff=True,
+            )
+            user.save_to_db()
+            user_profile = UserProfileModel(user_id=user.id)
+            user_profile.save_to_db()
 
 
 admin_username = input("Choose admin username: ")
