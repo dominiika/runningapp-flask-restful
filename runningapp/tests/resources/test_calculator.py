52b4e3d4501a2ace1a0273630bd067c9900fd122
Dosia <dominika.gajda@gmail.com>
2021-01-17 14:11:37 +0100

    Refactor Calculator Tests

--- runningapp/tests/resources/test_calculator.py
+++ runningapp/tests/resources/test_calculator.py
@@ -4,8 +4,7 @@ from runningapp import create_app
 from runningapp.tests.base_classes import BaseApp
 
 
-# TODO REFACTOR ALL THE TESTS
-class CalculatorTests(unittest.TestCase, BaseApp):
+class CalculatorTest(unittest.TestCase, BaseApp):
     """Test calculators"""
 
     def setUp(self) -> None:
@@ -13,67 +12,53 @@ class CalculatorTests(unittest.TestCase, BaseApp):
         self.app = self._set_up_test_app(create_app)
         self.client = self._set_up_client(self.app)
 
-    def test_bmi_calculator_status_code(self):
-        """Test if the status code is 201 if the user enters required data"""
-        data = {"height": 170, "weight": 100}
-        response = self.client.post(
-            path="/bmi",
-            data=json.dumps(data),
-            headers={"Content-Type": "application/json"},
-        )
+    def test_calculates_bmi_correctly(self):
+        self.__given_test_data_is_prepared()
 
-        self.assertEqual(response.status_code, 201)
+        self.__when_post_request_is_sent("bmi")
 
-    def test_bmi_calculator_data(self):
-        """Test if the data is returned correctly"""
-        data = {"height": 170, "weight": 100}
-        response = self.client.post(
-            path="/bmi",
-            data=json.dumps(data),
-            headers={"Content-Type": "application/json"},
-        )
+        self.__then_status_code_is_201_created()
+        self.__then_expected_bmi_is_returned()
 
+    def __then_expected_bmi_is_returned(self):
         expected_bmi = 34.6
+        self.assertEqual(self.response.json["bmi"], expected_bmi)
 
-        self.assertEqual(response.json["bmi"], expected_bmi)
+    def __then_status_code_is_201_created(self):
+        self.assertEqual(self.response.status_code, 201)
 
-    def test_caloric_needs_calculator_status_code(self):
-        """Test if the status code is 201 if the user enters required data"""
-        data = {
-            "height": 170,
-            "weight": 58,
-            "age": 26,
-            "gender": "Female",
-            "trainings_per_week": 5,
-        }
-        response = self.client.post(
-            path="/daily-calories",
-            data=json.dumps(data),
+    def __when_post_request_is_sent(self, path):
+        self.response = self.client.post(
+            path=f"/{path}",
+            data=json.dumps(self.data),
             headers={"Content-Type": "application/json"},
         )
 
-        self.assertEqual(response.status_code, 201)
+    def __given_test_data_is_prepared(self):
+        self.data = {"height": 170, "weight": 100}
 
-    def test_caloric_needs_calculator_data(self):
-        """Test if the data is returned correctly"""
-        data = {
-            "height": 170,
-            "weight": 58,
-            "age": 26,
-            "gender": "Female",
-            "trainings_per_week": 5,
-        }
-        response = self.client.post(
-            path="/daily-calories",
-            data=json.dumps(data),
-            headers={"Content-Type": "application/json"},
-        )
+    def test_returns_caloric_needs_correctly(self):
+        self.__given_caloric_needs_data_is_prepared()
 
+        self.__when_post_request_is_sent("daily-calories")
+
+        self.__then_status_code_is_201_created()
+        self.__then_expected_caloric_needs_are_returned()
+
+    def __then_expected_caloric_needs_are_returned(self):
         expected_caloric_needs = 2512
-
         self.assertEqual(
-            response.json["daily_caloric_needs"], expected_caloric_needs
+            self.response.json["daily_caloric_needs"], expected_caloric_needs
         )
+
+    def __given_caloric_needs_data_is_prepared(self):
+        self.data = {
+            "height": 170,
+            "weight": 58,
+            "age": 26,
+            "gender": "Female",
+            "trainings_per_week": 5,
+        }
 
 
 if __name__ == "__main__":
