3be824aae53de47cac2da3661301e14c31b0cdcb
Dosia <dominika.gajda@gmail.com>
2021-03-02 20:41:14 +0100

    Refactor kilometers run and registered users tests

--- runningapp/tests/resources/test_stats.py
+++ runningapp/tests/resources/test_stats.py
@@ -20,54 +20,52 @@ class StatsTests(unittest.TestCase, BaseApp, BaseUser, BaseDb, BaseTraining):
         self.app = self._set_up_test_app(create_app)
         self.client = self._set_up_client(self.app)
         self._set_up_test_db(db)
+        # self.user1 = self._create_sample_user(username="user1")
+        # self.user2 = self._create_sample_user(username="user2")
+
+    def test_returns_registered_users(self):
+        self.__given_users_are_created()
+
+        self.__when_get_request_is_sent_on('/total-users-number')
+
+        self.__then_status_code_will_be_200_ok()
+        self.__then_expected_registered_users_data_will_be_returned()
+
+    def __given_users_are_created(self):
         self.user1 = self._create_sample_user(username="user1")
         self.user2 = self._create_sample_user(username="user2")
 
-    def test_registered_users_status_code_ok(self):
-        """Test if the status code is 200"""
+    def __then_status_code_will_be_200_ok(self):
+        self.assertEqual(self.response.status_code, 200)
 
-        response = self.client.get(
-            path="/total-users-number",
+    def __when_get_request_is_sent_on(self, url):
+        self.response = self.client.get(
+            path=url,
             headers={"Content-Type": "application/json"},
         )
 
-        self.assertEqual(response.status_code, 200)
-
-    def test_registered_users_data(self):
-        """Test if the data is returned correctly"""
-
-        response = self.client.get(
-            path="/total-users-number",
-            headers={"Content-Type": "application/json"},
-        )
-
+    def __then_expected_registered_users_data_will_be_returned(self, expected_number):
         expected_number = 2
 
-        self.assertEqual(response.json["users_number"], expected_number)
+        self.assertEqual(self.response.json["users_number"], expected_number)
 
-    def test_kilometers_run_code_ok(self):
-        """Test if the status code is 200"""
+    def test_returns_kilometers_run(self):
+        self.__given_users_are_created()
+        self.__given_trainings_are_created()
 
-        response = self.client.get(
-            path="/total-kilometers-number",
-            headers={"Content-Type": "application/json"},
-        )
+        self.__when_get_request_is_sent_on("/total-kilometers-number")
 
-        self.assertEqual(response.status_code, 200)
+        self.__then_status_code_will_be_200_ok()
+        self.__then_expected_kilometers_data_will_be_returned()
 
-    def test_kilometers_run_data(self):
-        """Test if the data is returned correctly"""
-        self._create_sample_training(user=self.user1, distance=12)
-        self._create_sample_training(user=self.user2, distance=10)
-
-        response = self.client.get(
-            path="/total-kilometers-number",
-            headers={"Content-Type": "application/json"},
-        )
-
+    def __then_expected_kilometers_data_will_be_returned(self):
         expected_number = 22
 
-        self.assertEqual(response.json["kilometers_number"], expected_number)
+        self.assertEqual(self.response.json["kilometers_number"], expected_number)
+
+    def __given_trainings_are_created(self):
+        self._create_sample_training(user=self.user1, distance=12)
+        self._create_sample_training(user=self.user2, distance=10)
 
     def test_calories_burnt_code_ok(self):
         """Test if the status code is 200"""
