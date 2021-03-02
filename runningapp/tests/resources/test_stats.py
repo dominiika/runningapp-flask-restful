4beac2aaab4056ff47fe6c16072f5e3597f95777
Dosia <dominika.gajda@gmail.com>
2021-03-02 20:46:14 +0100

    black

--- runningapp/tests/resources/test_stats.py
+++ runningapp/tests/resources/test_stats.py
@@ -25,7 +25,7 @@ class StatsTests(unittest.TestCase, BaseApp, BaseUser, BaseDb, BaseTraining):
     def test_returns_registered_users(self):
         self.__given_users_are_created()
 
-        self.__when_get_request_is_sent_on('/total-users-number')
+        self.__when_get_request_is_sent_on("/total-users-number")
 
         self.__then_status_code_will_be_200_ok()
         self.__then_expected_registered_users_data_will_be_returned()
@@ -39,8 +39,7 @@ class StatsTests(unittest.TestCase, BaseApp, BaseUser, BaseDb, BaseTraining):
 
     def __when_get_request_is_sent_on(self, url):
         self.response = self.client.get(
-            path=url,
-            headers={"Content-Type": "application/json"},
+            path=url, headers={"Content-Type": "application/json"},
         )
 
     def __then_expected_registered_users_data_will_be_returned(self):
@@ -60,11 +59,17 @@ class StatsTests(unittest.TestCase, BaseApp, BaseUser, BaseDb, BaseTraining):
     def __then_expected_kilometers_data_will_be_returned(self):
         expected_number = 22
 
-        self.assertEqual(self.response.json["kilometers_number"], expected_number)
+        self.assertEqual(
+            self.response.json["kilometers_number"], expected_number
+        )
 
     def __given_trainings_are_created(self):
-        self._create_sample_training(user=self.user1, distance=12, time_in_seconds=3700)
-        self._create_sample_training(user=self.user2, distance=10, time_in_seconds=3800)
+        self._create_sample_training(
+            user=self.user1, distance=12, time_in_seconds=3700
+        )
+        self._create_sample_training(
+            user=self.user2, distance=10, time_in_seconds=3800
+        )
 
     def test_returns_calories_burnt(self):
         self.__given_users_are_created()
@@ -78,7 +83,9 @@ class StatsTests(unittest.TestCase, BaseApp, BaseUser, BaseDb, BaseTraining):
     def __then_expected_calories_data_will_be_returned(self):
         expected_calories = TrainingModel.calculate_total_calories()
 
-        self.assertEqual(expected_calories, self.response.json["calories_number"])
+        self.assertEqual(
+            expected_calories, self.response.json["calories_number"]
+        )
 
 
 if __name__ == "__main__":
